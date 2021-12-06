da387fddb53303c05289ac1409c1a8306b2e1389
xiafan <1195118980@qq.com>
2021-12-06 10:48:21 +0800

    添加日志工具类

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from utils.Log import Log
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@Log()
+def my_sum(x, y):
+    s = 0
+    for i in range(x, y + 1):
+        s += i
+    return s
 
-import lg
+
+# 0import lg
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # txt = bug.get_content('https://zzk.cnblogs.com/s/blogpost?Keywords=java&pageindex=1',)
     # result = bug.get_data(txt)
     # bug.write_data(result,'weather.csv')
-    # print(txt)
-    # print_hi('PyCharm')
+    # print(txt)10
+    # print_hi('Py+Charm')
     # sqliteConnection
     # sqliteConnection.test("夏凡")
-    lg.run_game()
-
+    # lg.run_game()
+    my_sum(1, 9999999)
---
+++ utils/Log.py
@@ -0,0 +1,30 @@
+﻿import datetime
+from functools import wraps
+
+
+class Log:
+    def __init__(self, path=None):
+        self._output = path
+
+    def __call__(self, f):  # 相当于原来的 inner_decorator
+        @wraps(f)
+        def wrap_func(*args, **kwargs):  # 增加了输入参数
+            now = datetime.datetime.now()
+            msg = now.strftime("%Y-%m-%d %H:%M:%S")  # 运行时刻
+            msg += f" {f.__name__}()\n"  # 运行的函数名
+
+            ret = f(*args, **kwargs)  # 透传了输入参数，并记录了输出
+
+            aft = datetime.datetime.now()
+            time_cost = aft - now
+            ms = time_cost.total_seconds() * 10 ** 3  # 毫秒
+            msg += now.strftime("%Y-%m-%d %H:%M:%S")
+            msg += f" {f.__name__}() return, cost {ms} ms"
+            if self._output is None:
+                print(msg)
+            else:
+                print(f"print logs into {self._output}")
+                with open(self._output, 'a+') as fp:
+                    fp.write(msg + '\n')
+
+        return wrap_func
